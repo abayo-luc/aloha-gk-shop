7f27259c2815462c41aa7bf19ab252fc4e64acf9
Luc <luc.bayo@gmail.com>
2020-08-17 13:57:09 +0200

    :sparkles: inital commit

---
+++ components/Header/TopHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <div style="background-color: #F5F5F5; padding: 24px;">
+    <a-page-header
+      :ghost="false"
+      title="AlohaGK"
+    >
+      <template slot="extra">
+        <a-badge count="0" :offset="[-2,10]">
+          <a-button shape="circle" type="dashed">
+            <a-icon type="shopping-cart" />
+          </a-button>
+        </a-badge>
+
+        <a-button key="2" icon="user-add">
+          SignUp
+        </a-button>
+        <a-button key="1" type="primary" icon="user">
+          Login
+        </a-button>
+      </template>
+    </a-page-header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ToHeader'
+}
+</script>
+
+<style>
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <top-header />
+  </div>
+</template>
+<script>
+import TopHeader from '@/components/Header/TopHeader.vue'
+export default {
+  components: {
+    TopHeader
+  }
+}
+</script>
+
+<style>
+
+</style>
