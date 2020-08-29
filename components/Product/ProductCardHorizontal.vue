700cdf2ef583a09568b86064a98b931015b52372
Luc <luc.bayo@gmail.com>
2020-08-29 20:54:31 +0200

    feature: add products page

---
+++ components/Product/ProductCardHorizontal.vue
@@ -0,0 +1,156 @@
+<template>
+  <div class="row row_item">
+    <div class="col-sm-4">
+      <figure>
+        <span class="ribbon off">-30%</span>
+        <a href="product-detail-1.html">
+          <img class="img-fluid lazy loaded" src="https://previews.dropbox.com/p/thumb/AA6dbMBjITuPC7ER_nh3v2dfRrYqiT_JO_WYitEiq28O6F3HvPAytSXBePAqvM6pXFpSq0eblDcXyCxc-vpKWIc3PhoPUzcRk3YbCb2UYomBbTXvZBK7EntzkLkqJtrfrsk3LVGBBv5f2VpovPIwyhQ4EqRbzIrTPSb1tiG7cmT8FyogrLr76WiLHEo20co6iHJ07_FB_f_ayaOtJFcTfFp4cmjeWLIoeG7eGnzF9VSOGk1Vo98l8N_qokMystFMXih5zVzZa2dfXAQUUporpvd2YXWvpv_aA-wUdePJL_2bxlwA_hsMupDo3ofOP12jETHaKRvcgS837cmwoubPYrQl-PVUzPGfIFb6SEChALNFKg/p.jpeg?fv_content=true&size_mode=5" data-src="img/products/shoes/1.jpg" alt="" data-was-processed="true">
+        </a>
+      </figure>
+    </div>
+    <div class="col-sm-8">
+      <div class="rating">
+        <a-icon type="star" class="icon-star voted" theme="filled" />
+        <a-icon type="star" class="icon-star voted" theme="filled" />
+        <a-icon type="star" class="icon-star voted" theme="filled" />
+        <a-icon type="star" class="icon-star voted" theme="filled" />
+        <a-icon type="star" />
+      </div>
+      <a href="product-detail-1.html">
+        <h3>Armor Air x Fear</h3>
+      </a>
+      <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident...</p>
+      <div class="price_box">
+        <span class="new_price">$48.00</span>
+        <span class="old_price">$60.00</span>
+      </div>
+      <ul>
+        <li>
+          <a-button type="primary">
+            Add to cart
+          </a-button>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductCardHorizontal'
+}
+</script>
+
+<style scoped>
+.row_item{
+    margin-bottom: 30px;
+}
+.row_item figure {
+    position: relative;
+    margin-bottom: 0;
+    -webkit-box-shadow: 0 20px 20px -20px rgba(0, 0, 0, 0.25);
+    -moz-box-shadow: 0 20px 20px -20px rgba(0, 0, 0, 0.25);
+    box-shadow: 0 20px 20px -20px rgba(0, 0, 0, 0.25);
+}
+.ribbon.off {
+    background-color: #F33;
+}
+.ribbon.off:before {
+    border-color: #F33 transparent transparent;
+}
+.ribbon:before {
+    border-style: solid solid solid none;
+    border-width: 9px 9px 9px 0;
+    bottom: -15px;
+    content: "";
+    left: 0;
+    position: absolute;
+    width: 9px;
+}
+.ribbon {
+    color: #fff;
+    display: inline-block;
+    font-size: 11px;
+    font-size: 0.6875rem;
+    left: 10px;
+    line-height: 1;
+    position: absolute;
+    text-align: center;
+    text-transform: uppercase;
+    top: 10px;
+    padding: 7px 10px;
+    font-weight: 600;
+    min-width: 45px;
+    z-index: 1;
+}
+.row_item .rating {
+    margin-bottom: 5px;
+}
+.rating i.voted {
+    color: #fec348;
+}
+.row_item a h3 {
+    font-size: 18px;
+    font-size: 1.125rem;
+    color: #444;
+}
+.row_item p {
+    margin-bottom: 20px;
+}
+.row_item .price_box {
+    margin-bottom: 20px;
+}
+.new_price {
+    color: #004dda;
+    font-weight: 500;
+    font-size: 1.3125rem;
+}
+.old_price {
+    color: #999;
+    text-decoration: line-through;
+    display: inline-block;
+    white-space: nowrap;
+    font-weight: 500;
+    font-size: 16px;
+    font-size: 1rem;
+}
+.row_item ul {
+    margin: 15px 0 0 0;
+    padding: 0;
+    list-style: none;
+}
+.row_item ul {
+    margin: 15px 0 0 0;
+    padding: 0;
+    list-style: none;
+}
+.row_item ul li {
+    display: inline-block;
+}
+
+a.btn_1, .btn_1 {
+    border: none;
+    color: #fff;
+    background: #004dda;
+    outline: none;
+    cursor: pointer;
+    display: inline-block;
+    text-decoration: none;
+    padding: 12px 25px;
+    color: #fff;
+    font-weight: 500;
+    text-align: center;
+    font-size: 14px;
+    font-size: 0.875rem;
+    -moz-transition: all 0.3s ease-in-out;
+    -o-transition: all 0.3s ease-in-out;
+    -webkit-transition: all 0.3s ease-in-out;
+    -ms-transition: all 0.3s ease-in-out;
+    transition: all 0.3s ease-in-out;
+    -webkit-border-radius: 3px;
+    -moz-border-radius: 3px;
+    -ms-border-radius: 3px;
+    border-radius: 3px;
+    line-height: normal;
+}
+</style>
---
+++ pages/products/components/TopBanner.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="top_banner">
+    <div class="opacity-mask d-flex align-items-center" data-opacity-mask="rgba(0, 0, 0, 0.3)" style="background-color: rgba(0, 0, 0, 0.3);">
+      <div class="container">
+        <div class="breadcrumbs">
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Category</a></li>
+            <li>Page active</li>
+          </ul>
+        </div>
+        <h1>Shoes - Grid listing</h1>
+      </div>
+    </div>
+    <img src="https://previews.dropbox.com/p/thumb/AA5vc5mgqzJrKt6E-lMUl4vS9Z6c7KYaYl8Fh3dLV_Gh1v3xpbTtW_cGcrvRUU3UVZeAO-pyX0zxfM7tA0JDYGHla_L-f0UGWgvTxyQQDPp86sngGZ9QQ8L_E28wcM6oqEw6UWjNlc1xZZ1ZCpY9VSiWAZtw56aR6t5qTYtL8IHwsU4eLzitUa0S47B9XAxy-pt4QccDqV9oeOnJp2RN7w2XsAHEC-vH6SU18yLmNtidE_DLkvkVR3dHqtzj22bFGSAWDGgWm_yKItar5uKCa_6K0g0MOOlvwLl9DCdyZbZ81BVyfLwDakZhDUjwJZsKtXNrdrSew-VMVEzcz-3aa9JcMhmhFf3-mpJRUzi-EyHwkQ/p.jpeg?fv_content=true&size_mode=5" class="img-fluid" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TopBanner'
+}
+</script>
+
+<style lang="css" scoped>
+.top_banner .breadcrumbs ul li a {
+    color: #fff;
+    opacity: 0.6;
+}
+.top_banner {
+    height: 250px;
+    overflow: hidden;
+    position: relative;
+}
+.opacity-mask {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    left: 0;
+    top: 0;
+    z-index: 2;
+}
+.top_banner .breadcrumbs {
+    color: #fff;
+}
+
+.breadcrumbs ul {
+    margin: 0 0 10px 0;
+    padding: 0;
+    font-size: 12px;
+    font-size: 0.75rem;
+}
+.breadcrumbs ul li {
+    display: inline-block;
+    margin-right: 7px;
+    padding-right: 8px;
+    position: relative;
+}
+.top_banner h1 {
+    font-size: 32px;
+    font-size: 2rem;
+    font-weight: 900;
+    text-transform: uppercase;
+    color: #fff;
+}
+.top_banner img {
+    z-index: 1;
+    width: 100%;
+    height: auto;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    -webkit-transform: translate(-50%, -50%);
+    -moz-transform: translate(-50%, -50%);
+    -ms-transform: translate(-50%, -50%);
+    -o-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+}
+</style>
